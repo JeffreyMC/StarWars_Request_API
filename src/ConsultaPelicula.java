f91ac7fb2249e430d0b7c47fa5baf3c3b255e163
Jeffrey MC <jeffmc@duck.com>
2024-10-03 12:14:42 -0600

    programa terminado. Guarda JSON de la API en archivos ,json

--- src/ConsultaPelicula.java
+++ src/ConsultaPelicula.java
@@ -7,20 +7,20 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
-    Pelicula buscaPelicula(int numeroPelicula){
+    public Pelicula buscaPelicula(int numeroPelicula){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://swapi.dev/api/films/" + numeroPelicula))
                 .build();
 
-        HttpResponse<String> response = null;
         try {
-            response = client.send(request,
+            HttpResponse <String> response = client.send(request,
                     HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontre esa pelicula.");
         }
 
-        return new Gson().fromJson(response.body(), Pelicula.class);
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         ConsultaPelicula consulta = new ConsultaPelicula();
-        Pelicula pelicula = consulta.buscaPelicula(1);
 
-        System.out.println(pelicula);
+        Scanner escritura = new Scanner(System.in);
+        System.out.print("Escriba el numero de la pelicula de Star Wars a consultar: ");
+
+        try{
+            var numeroPelicula = Integer.valueOf(escritura.nextLine());
+
+            Pelicula pelicula = consulta.buscaPelicula(numeroPelicula);
+
+            System.out.println(pelicula);
+
+            GeneradorDeArchivo crearArchivo = new GeneradorDeArchivo();
+            crearArchivo.guardarJson(pelicula);
+
+        }catch (NumberFormatException e){
+            System.out.println("Numero no encontrado: " + e.getMessage());
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());;
+            System.out.println("Finalizando la aplicacion...");
+        }
     }
 }
